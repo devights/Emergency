31df2a0eceec4543c65381434623a49c7d2ab7fb
Stephen De Vight <devights@gmail.com>
2012-02-27 20:02:30 -0800

    Database schema, fixes #2

---
+++ data/app.sql
@@ -0,0 +1,47 @@
+CREATE TABLE Incident (
+	id CHAR(10) NOT NULL,
+	start TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	end TIMESTAMP NULL,
+	location TINYTEXT NOT NULL,
+	type_id INT NOT NULL,
+	level INT NOT NULL,
+	PRIMARY KEY (id)
+); 
+
+CREATE TABLE Vehicle (
+	id INT AUTO_INCREMENT,
+	name VARCHAR(8) NOT NULL,
+	type_id INT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IncidentType (
+	id INT AUTO_INCREMENT,
+	name TINYTEXT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Dispatch (
+	vehicle_id INT NOT NULL,
+	incident_id CHAR(10) NOT NULL,
+	PRIMARY KEY (vehicle_id, incident_id)
+);
+
+CREATE TABLE Station (
+	id INT AUTO_INCREMENT,
+	name TINYTEXT NOT NULL,
+	location TINYTEXT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Assignment (
+	vehicle_id INT NOT NULL,
+	station_id INT NOT NULL,
+	PRIMARY KEY (vehicle_id, station_id)
+);
+
+CREATE TABLE VehicleType (
+	vehicle_id INT NOT NULL,
+	name TINYTEXT NOT NULL,
+	PRIMARY KEY (vehicle_id)
+);
